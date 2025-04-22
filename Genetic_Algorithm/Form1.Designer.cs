bb5bec387442be543bc6373826235f0bbb3cdbd1
rumeysayrtsvr <146195503+rumeysayrtsvr@users.noreply.github.com>
2025-04-22 12:41:06 +0300

    Add files via upload

---
+++ Genetic_Algorithm/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Genetic_Algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalistir = new System.Windows.Forms.Button();
+            this.txtGenerations = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtElitismRate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMutationRate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCrossoverRate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPopulationSize = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.163636F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(441, 445);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(397, 123);
+            this.listBox1.TabIndex = 46;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(733, 365);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(105, 24);
+            this.label13.TabIndex = 43;
+            this.label13.Text = "                   ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label12.Location = new System.Drawing.Point(437, 365);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(290, 24);
+            this.label12.TabIndex = 42;
+            this.label12.Text = "En iyi Amaç Fonksiyonu(Fitness):";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(637, 308);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(105, 24);
+            this.label11.TabIndex = 41;
+            this.label11.Text = "                   ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label10.Location = new System.Drawing.Point(437, 308);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(194, 24);
+            this.label10.TabIndex = 40;
+            this.label10.Text = "En iyi sonuç Y değeri:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(639, 253);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 24);
+            this.label9.TabIndex = 39;
+            this.label9.Text = "                   ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label8.Location = new System.Drawing.Point(437, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(196, 24);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "En iyi sonuç X değeri:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(26, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 24);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Yakınsama Grafiği:";
+            // 
+            // btnCalistir
+            // 
+            this.btnCalistir.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCalistir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCalistir.Location = new System.Drawing.Point(291, 161);
+            this.btnCalistir.Name = "btnCalistir";
+            this.btnCalistir.Size = new System.Drawing.Size(240, 60);
+            this.btnCalistir.TabIndex = 34;
+            this.btnCalistir.Text = "GENETİK ALGORİTMAYI ÇALIŞTIR";
+            this.btnCalistir.UseVisualStyleBackColor = false;
+            this.btnCalistir.Click += new System.EventHandler(this.btnCalistir_Click);
+            // 
+            // txtGenerations
+            // 
+            this.txtGenerations.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtGenerations.Location = new System.Drawing.Point(596, 71);
+            this.txtGenerations.Multiline = true;
+            this.txtGenerations.Name = "txtGenerations";
+            this.txtGenerations.Size = new System.Drawing.Size(147, 31);
+            this.txtGenerations.TabIndex = 33;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(424, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 24);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Jenerasyon Sayısı:";
+            // 
+            // txtElitismRate
+            // 
+            this.txtElitismRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtElitismRate.Location = new System.Drawing.Point(209, 108);
+            this.txtElitismRate.Multiline = true;
+            this.txtElitismRate.Name = "txtElitismRate";
+            this.txtElitismRate.Size = new System.Drawing.Size(147, 31);
+            this.txtElitismRate.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(26, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 24);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Seçkinlik Oranı:";
+            // 
+            // txtMutationRate
+            // 
+            this.txtMutationRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMutationRate.Location = new System.Drawing.Point(209, 67);
+            this.txtMutationRate.Multiline = true;
+            this.txtMutationRate.Name = "txtMutationRate";
+            this.txtMutationRate.Size = new System.Drawing.Size(147, 31);
+            this.txtMutationRate.TabIndex = 29;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(26, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 24);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Mutasyon Oranı:";
+            // 
+            // txtCrossoverRate
+            // 
+            this.txtCrossoverRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCrossoverRate.Location = new System.Drawing.Point(596, 28);
+            this.txtCrossoverRate.Multiline = true;
+            this.txtCrossoverRate.Name = "txtCrossoverRate";
+            this.txtCrossoverRate.Size = new System.Drawing.Size(147, 31);
+            this.txtCrossoverRate.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(425, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 24);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Çaprazlama Oranı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 24);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Popülasyon Boyutu:";
+            // 
+            // txtPopulationSize
+            // 
+            this.txtPopulationSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPopulationSize.Location = new System.Drawing.Point(209, 28);
+            this.txtPopulationSize.Multiline = true;
+            this.txtPopulationSize.Name = "txtPopulationSize";
+            this.txtPopulationSize.Size = new System.Drawing.Size(147, 31);
+            this.txtPopulationSize.TabIndex = 24;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(30, 253);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(315, 313);
+            this.chart1.TabIndex = 47;
+            this.chart1.Text = "chart1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.78182F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(437, 418);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 24);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "Sonuç Geçmişi:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(878, 600);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalistir);
+            this.Controls.Add(this.txtGenerations);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtElitismRate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMutationRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCrossoverRate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPopulationSize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalistir;
+        private System.Windows.Forms.TextBox txtGenerations;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtElitismRate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMutationRate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCrossoverRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPopulationSize;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Genetic_Algorithm/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Genetic_Algorithm
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private List<Individual> population;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalistir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int populationSize = int.Parse(txtPopulationSize.Text);
+                double crossoverRate = double.Parse(txtCrossoverRate.Text);
+                double mutationRate = double.Parse(txtMutationRate.Text);
+                double elitismRate = double.Parse(txtElitismRate.Text);
+                int generationCount = int.Parse(txtGenerations.Text);
+
+            
+                if (populationSize <= 0) throw new Exception("Popülasyon boyutu pozitif bir tamsayı olmalı.");
+                if (crossoverRate < 0 || crossoverRate > 1)throw new Exception("Çaprazlama oranı 0 ile 1 arasında olmalı.");
+                if (mutationRate < 0 || mutationRate > 1)throw new Exception("Mutasyon oranı 0 ile 1 arasında olmalı.");
+                if (elitismRate < 0 || elitismRate > 1) throw new Exception("Seçkinlik oranı 0 ile 1 arasında olmalı.");     
+                if (generationCount <= 0)throw new Exception("Jenerasyon sayısı pozitif bir tamsayı olmalı.");
+
+                    
+           
+                RunGeneticAlgorithm(populationSize, crossoverRate, mutationRate, elitismRate, generationCount);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Geçersiz Girdi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void RunGeneticAlgorithm(int populationSize, double crossoverRate, double mutationRate, double elitismRate, int generationCount)
+        {
+            population = InitializePopulation(populationSize);
+            chart1.Series.Clear();
+
+            Series series = new Series
+            {
+                ChartType = SeriesChartType.Line,
+                Name = "Fitness Value"
+            };
+            chart1.Series.Add(series);
+
+            for (int gen = 0; gen < generationCount; gen++)
+            {
+                List<Individual> newPopulation = new List<Individual>();
+                int eliteCount = (int)(populationSize * elitismRate);
+
+              
+                var elites = population.OrderBy(i => i.Fitness()).Take(eliteCount).ToList();
+                newPopulation.AddRange(elites);
+
+               
+                while (newPopulation.Count < populationSize)
+                {
+                    Individual parent1 = TournamentSelection();
+                    Individual parent2 = TournamentSelection();
+                    Individual offspring = Crossover(parent1, parent2, crossoverRate);
+                    Mutate(offspring, mutationRate);
+                    newPopulation.Add(offspring);
+                }
+
+               
+                population = newPopulation.OrderBy(i => i.Fitness()).ToList();
+                series.Points.AddXY(gen, population.First().Fitness());
+            }
+
+          
+            Individual bestIndividual = population.First();
+            listBox1.Items.Add($"{DateTime.Now:HH:mm:ss} → X = {bestIndividual.X:F4}, Y = {bestIndividual.Y:F4}, Fitness = {bestIndividual.Fitness():F4}");
+            label9.Text = $" {bestIndividual.X:F4}";
+            label11.Text = $" {bestIndividual.Y:F4}";
+            label13.Text = $" {bestIndividual.Fitness():F4}";
+            
+        }
+
+        private List<Individual> InitializePopulation(int populationSize)
+        {
+            List<Individual> pop = new List<Individual>();
+            for (int i = 0; i < populationSize; i++)
+            {
+                double x = random.NextDouble() * 4 - 2; 
+                double y = random.NextDouble() * 4 - 2; 
+                pop.Add(new Individual(x, y));
+            }
+            return pop;
+        }
+
+        private Individual TournamentSelection()
+        {
+            int tournamentSize = 4;
+            List<Individual> tournament = new List<Individual>();
+            for (int i = 0; i < tournamentSize; i++)
+            {
+                tournament.Add(population[random.Next(population.Count)]);
+            }
+            return tournament.OrderBy(i => i.Fitness()).First();
+        }
+
+        private Individual Crossover(Individual parent1, Individual parent2, double crossoverRate)
+        {
+            if (random.NextDouble() < crossoverRate)
+            {
+               
+                double newX = (parent1.X + parent2.X) / 2;
+                double newY = (parent1.Y + parent2.Y) / 2;
+                return new Individual(newX, newY);
+            }
+            return new Individual(parent1.X, parent1.Y);
+        }
+
+        private void Mutate(Individual individual, double mutationRate)
+        {
+            if (random.NextDouble() < mutationRate)
+            {
+               
+                individual.X += random.NextDouble() * 2 - 1;
+                individual.Y += random.NextDouble() * 2 - 1;
+
+               
+                individual.X = Math.Max(-2, Math.Min(2, individual.X));
+                individual.Y = Math.Max(-2, Math.Min(2, individual.Y));
+            }
+        }
+    }
+
+    public class Individual
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Individual(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+        public double Fitness()
+        {
+            double term1 = 1 + Math.Pow(X + Y + 1, 2) * (19 - 14 * X + 3 * Math.Pow(X, 2) - 14 * Y + 6 * X * Y + 3 * Math.Pow(Y, 2));
+            double term2 = 30 + Math.Pow(2 * X - 3 * Y, 2) * (18 - 32 * X + 12 * Math.Pow(X, 2) + 48 * Y - 36 * X * Y + 27 * Math.Pow(Y, 2));
+            return term1 * term2;
+        }
+    }
+}
